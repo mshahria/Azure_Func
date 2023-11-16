d3e3720aa8fce58d4520e383af403b5e96e6ac1b
Md Tanzil Shahria <mshahria@uwm.edu>
2023-11-16 13:53:41 -0600

    Added execution time to the function response

--- DoubleNumbersFunction/__init__.py
+++ DoubleNumbersFunction/__init__.py
@@ -1,8 +1,18 @@
 import azure.functions as func
 import json
+import time
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
+    start_time = time.time()
+
     data = req.get_json()
     numbers = data.get("numbers", [])
     result = [x * 2 for x in numbers]
-    return func.HttpResponse(json.dumps({"result": result}), mimetype="application/json")
+
+    execution_time = time.time() - start_time
+    response_data = {
+        "result": result,
+        "executionTime": execution_time
+    }
+
+    return func.HttpResponse(json.dumps(response_data), mimetype="application/json")
